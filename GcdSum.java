3306762ce2e5e5796731a85a39900b5d17149eb4
Behera <BBEHERA@fnc.net.local>
2019-09-23 18:15:23 +0530

    remove space on gcdsum file

--- GcdSum.java
+++ GcdSum.java
@@ -41,8 +41,7 @@ public static int gcdSum(int a[],int memo[][],int k,int i,int j)
 		int memo[][]=new int[j+1][j+1];
 		int k=4;
 		for(int i=0;i<j-(k-2);i++)
-		gcdSum(a,memo,k,i,j);
-		
+		gcdSum(a,memo,k,i,j);		
 		Arrays.stream(memo).forEach(x->System.out.println(Arrays.toString(x)));
 		
 		//System.out.println(Arrays.toString(memo));
