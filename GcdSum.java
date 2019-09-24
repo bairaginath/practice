c951a59f6a3c51da0c447b4ffd50abb8ca2a5c69
bairaginath <bairaginath2013@gmail.com>
2019-09-24 20:08:20 +0530

    GcdSum part1

--- GcdSum.java
+++ GcdSum.java
@@ -3,14 +3,12 @@ import java.util.*;
 
 public class GcdSum {
 	
-	public static int gcd(int x,int y)
-	{
-	  int max=Math.max(x,y);
-	  int min=Math.min(x,y);
-	  if(max%min==0)
-		  return min;
-	  return gcd(max,max%min);
-	}
+	public static int gcd(int a, int b) 
+    { 
+      if (b == 0) 
+        return a; 
+      return gcd(b, a % b);  
+    } 
 	  
 	
 	
@@ -37,12 +35,37 @@ public static int gcdSum(int a[],int memo[][],int k,int i,int j)
 	
 	public static void main(String args[]){
 		int a[]={4,5,3,7,8,10,5,8};
-		int j=a.length-1;
-		int memo[][]=new int[j+1][j+1];
-		int k=4;
-		for(int i=0;i<j-(k-2);i++)
-		gcdSum(a,memo,k,i,j);		
-		Arrays.stream(memo).forEach(x->System.out.println(Arrays.toString(x)));		
+		//int j=a.length-1;
+		int K=4;
+		int memo[][]=new int[(K/2)+1][a.length];
+		int jump[][]=new int[(K/2)+1][a.length];
+		for(int k=2;k<=K;k=k+2){
+			for(int i=a.length-2;i>=0;i--)
+			{ 
+		      int max=memo[(k/2)][i+1];
+			  int index=jump[(k/2)][i+1];
+			  int prevMax=max;
+			  for(int j=i+1;j<a.length-(k-2);j++)
+			  {  
+		        int g=gcd(a[i],a[j]);				
+		         max=Math.max(max,g+memo[k/2-1][j==a.length-1?j:j+1]);
+				 index=max!=prevMax?j:index;
+				 prevMax=max;
+				 System.out.println("i "+i+" j "+j+" k "+k+" g "+g+" max"+max+" index"+index);
+			  }
+			  memo[k/2][i]=max;
+			  jump[k/2][i]=index;
+			}
+		}
+			
+			
+			
+		
+		//for(int i=0;i<j-(k-2);i++)
+		//gcdSum(a,memo,k,i,j);		
+		Arrays.stream(memo).forEach(x->System.out.println(Arrays.toString(x)));
+        System.out.println("jump index");		
+		Arrays.stream(jump).forEach(x->System.out.println(Arrays.toString(x)));		
 		//System.out.println(Arrays.toString(memo));
 		
 		
