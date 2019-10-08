13b08ef4b4914d728cc8854dd89b720ff6f3617c
bairagi <mail2bairagi007@gmail.com>
2019-10-09 00:49:17 +0530

    Noway failed have to check logic

--- src/NoWayAverageSumK.java
+++ src/NoWayAverageSumK.java
@@ -4,27 +4,37 @@ import java.util.stream.IntStream;
 public class NoWayAverageSumK {
 
 
-    static int noWayAverageSum(int a[],int sum)
+    static int noWayAverageSum(int a[],int k,int sum)
     {
-
+        System.out.println(Arrays.toString(a));
+        if(a.length==1 && sum==a[0])
+            return 1;
+        if(a.length<=1)
+            return 0;
+//        int s=Arrays.stream(a).sum();
+//        if(s==sum)
+//            return 1;
+        int total=0;
         for(int i=a.length-1;i>=0;i--) {
             int b[]=new int[a.length-1];
             System.arraycopy(a,0,b,0,i);
             System.arraycopy(a,i+1,b,i,a.length-1-i);
-            System.out.println(Arrays.toString(b));
+            //System.out.println(Arrays.toString(b));
+            int x=noWayAverageSum(b,k,sum-a[i]);
+            int y=noWayAverageSum(b,k,k*b.length);
+            total+=(x+y);
         }
+        return total;
 
-
-
-          return 0;
 
     }
 
 
     public static void main(String[] args) {
-        int a[] = {3, 6, 2, 8, 7, 6, 5, 9};
+       int a[] = {3, 6, 2, 8, 7, 6, 5, 9};
+        //int a[] = {2,3};
         int K = 5;
         Arrays.sort(a);
-        System.out.println(noWayAverageSum(a,K*a.length));
+        System.out.println(noWayAverageSum(a,K,K*a.length));
     }
 }
