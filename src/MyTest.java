2c424d5d367b336b3326f64c13c568b177b2d761
bairagi <mail2bairagi007@gmail.com>
2020-02-09 04:02:37 +0530

    java collection notes

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,21 +1,29 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.LinkedHashMap;
 
 public class MyTest {
 
 	public static void main(String args[]) {
 		
-		LinkedHashMap<Integer,String> map=new LinkedHashMap<>(16, .75f, true);
-		map.put(1,"bairagi");
-		map.put(1,"bairagi");
-		map.put(2,"bairagi");
-		map.put(3,"bairagi");
-		map.put(4,"bairagi");
-		map.put(5,"bairagi");
-		
-		map.forEach((k,v)->System.out.println(k+" "+v));
-		map.get(3);
-		map.forEach((k,v)->System.out.println(k+" "+v));
+		Map<Integer, String> studentsById = new HashMap<>();
+		studentsById.put(1, "Foo");
+		studentsById.put(2, "Bar");
+		studentsById.put(3, "Baz");
+		studentsById.put(4, "Qux");
+		 
+		List<Map.Entry<Integer, String>> shuffledStudentEntries
+		 = new ArrayList<>(studentsById.entrySet());
+		Collections.shuffle(shuffledStudentEntries);
+		Map<Integer,String> aftershuffle=shuffledStudentEntries.stream()
+				.collect(Collectors.toMap(Map.Entry::getKey,Map.Entry::getValue,(v1,v2)->v1,
+						()->new LinkedHashMap<>()));
+		aftershuffle.entrySet().forEach(System.out::println);
 		
 
 	}
