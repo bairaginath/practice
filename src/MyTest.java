d7acf2aa7497d5911682e1005b8951e739a41b1e
bairaginath <bairaginath2013@gmail.com>
2020-02-11 19:40:13 +0530

    spring boot concept

--- src/MyTest.java
+++ src/MyTest.java
@@ -8,12 +8,28 @@ import java.util.stream.Collectors;
 import java.util.LinkedHashMap;
 
 public class MyTest {
+	
+	public int[] notAlone(int[] nums, int val) {
+		  int length=nums.length;
+		  
+		  int result[]=java.util.stream.IntStream.range(0,length).map(i->
+		    {
+		      if(i==0 || i==length-1)
+		         return nums[i];
+		      else if(nums[i]==val)
+		        return Math.max(nums[i-1],nums[i+1]);
+		      else
+		    	  return nums[i];
+			  
+		    }
+		  ).toArray();
+		  return result;
+		}
 
 	public static void main(String args[]) {
-		String joinedString = String.join("", new String[]{ "Convert", "With", "Java", "Streams" });
-        System.out.println(joinedString);		
+			
 
-        MutableIntIntMap mutableIntIntMap = IntIntMaps.mutable.empty();
+       
 	}
 
 }
