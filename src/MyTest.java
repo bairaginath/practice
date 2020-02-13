d4dff8a495cfec7190884703f7e587bee8617121
bairaginath <bairaginath2013@gmail.com>
2020-02-13 21:08:45 +0530

    spring boot actuator

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,35 +1,47 @@
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.LinkedHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
+
+import javafx.util.converter.CharacterStringConverter;
 
 public class MyTest {
-	
-	public int[] notAlone(int[] nums, int val) {
-		  int length=nums.length;
-		  
-		  int result[]=java.util.stream.IntStream.range(0,length).map(i->
-		    {
-		      if(i==0 || i==length-1)
-		         return nums[i];
-		      else if(nums[i]==val)
-		        return Math.max(nums[i-1],nums[i+1]);
-		      else
-		    	  return nums[i];
-			  
-		    }
-		  ).toArray();
-		  return result;
+
+	static String findLongestPalindrome(String str) {
+		
+		String s[] = new String[str.length()];
+		for (int i = 0; i < str.length(); i++) {
+			s[i] = String.valueOf(str.charAt(i));
 		}
+		Map<String, Long> map = Arrays.stream(s)
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+		String beg = "", mid = "", end = "";
+
+		for (String x : map.keySet()) {
+			if (map.get(x) % 2 == 1) {
+				mid = x;
+				map.put(x, map.get(x) - 1);
+			} else{
+				
+				for (int i = 0; i < map.get(x) / 2; i++)
+					beg += x;
+			}
+			System.out.println("mid="+mid+" beg="+beg);
+		}
+		end = beg;
+
+		end = new String(new StringBuilder(beg).reverse());
+		return beg + mid + end;
+
+	}
 
 	public static void main(String args[]) {
-			
+		System.out.println(findLongestPalindrome("abbaccd"));
+		Arrays.stream(new int[]{1,2,3,4});
+		"bairagi".chars().boxed().forEach(System.out::println);;
 
-       
 	}
 
 }
